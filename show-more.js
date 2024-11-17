c85e2ee983c20c0fd753141b6783963681a0b30c
Jordan <98762843+Squeishy@users.noreply.github.com>
2024-11-17 17:09:01 +0100

    SmallUpdate

---
+++ show-more.js
@@ -0,0 +1,4 @@
+document.getElementById('learn-more-button').addEventListener('click', function () {
+    const moreInfoDiv = document.getElementById('more-about-dashing-krew');
+    moreInfoDiv.classList.toggle('visible');
+});
