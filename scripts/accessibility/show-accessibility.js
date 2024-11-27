7b4acee0d6a7fa6f6920f6f9d7727ec395b78d6b
Jordan <98762843+Squeishy@users.noreply.github.com>
2024-11-27 18:50:48 +0100

    Header try to fix update
    🗿

--- scripts/accessibility/show-accessibility.js
+++ scripts/accessibility/show-accessibility.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const toggleMobileButton = document.getElementById("toggle-accessibility-mobile");
     const togglePCButton = document.getElementById("toggle-accessibility-pc");
 
+    const nav = document.querySelector('header nav');
+
     const header = document.querySelector("header");
     const headerNavLinks = document.getElementById("header-navigation-links");
     const accessibilityNav = document.getElementById("accessibility-nav");
@@ -14,10 +16,14 @@ document.addEventListener("DOMContentLoaded", function () {
             headerNavLinks.style.display = "none";
             accessibilityNav.style.display = "block";
             header.classList.add("expand");
-        } else {
+            console.log("show accessibility");
+        }
+        else if(!header.classList.contains("expand"))
+        {
             headerNavLinks.style.display = "flex";
             accessibilityNav.style.display = "none";
             header.classList.remove("expand");
+            console.log("header has expand");
         }
     }
 
--- scripts/toggle-header.js
+++ scripts/toggle-header.js
@@ -2,14 +2,32 @@ document.addEventListener('DOMContentLoaded', () => {
     const toggleButton = document.getElementById('toggle-header');
     const accessibilityButton = document.getElementById('toggle-accessibility-mobile');
     const nav = document.querySelector('header nav');
+    
+    const header = document.querySelector("header");
+    const headerNavLinks = document.getElementById("header-navigation-links");
+    const accessibilityNav = document.getElementById("accessibility-nav");
 
     // Toggle the 'active' class for both opening and closing the menu
     toggleButton.addEventListener('click', () => {
         nav.classList.toggle('active');
+
+        if (nav.classList.contains('active')) {
+            console.log("show menu");
+
+            headerNavLinks.style.display = "flex";
+            accessibilityNav.style.display = "none";
+            header.classList.remove("expand");
+            console.log("hide accessibility");
+        }
+        else
+        {
+            console.log("hide menu");
+        }
     });
 
     // Only add the 'active' class to open the menu, but do not remove it
     accessibilityButton.addEventListener('click', () => {
         nav.classList.add('active');
+        console.log("show menu from accessibility");
     });
 });
