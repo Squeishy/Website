a336f63302d87762162fed58eb44404328fa2084
Squeishy <gdlespainsaucho@gmail.com>
2024-12-01 23:55:46 +0100

    Finished press media kit translation

--- scripts/copy-to-clipboard.js
+++ scripts/copy-to-clipboard.js
@@ -2,15 +2,13 @@ const copyButtons = document.querySelectorAll(".color-info button");
 
 copyButtons.forEach((button) => {
     button.addEventListener("click", () => {
-        // Get the text to copy from the adjacent <p> element
         const colorCodeElement = button.previousElementSibling.querySelector("p");
         const colorCode = colorCodeElement.textContent;
 
-        // Copy the text to the clipboard
         navigator.clipboard.writeText(colorCode)
             .then(() => {
                 const originalText = button.textContent;
-                button.textContent = "Copied!";
+                button.textContent = "Copied !";
                 setTimeout(() => {
                     button.textContent = originalText;
                 }, 2000);
