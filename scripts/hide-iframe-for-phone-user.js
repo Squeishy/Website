f2788f3b38a458966825fd298637ba35d738fe0b
Jordan <98762843+Squeishy@users.noreply.github.com>
2024-12-02 11:47:03 +0100

    Tankerz Page update
    Me when when me when me when when me when

--- scripts/hide-iframe-for-phone-user.js
+++ scripts/hide-iframe-for-phone-user.js
@@ -1,6 +1,6 @@
 if (/Mobi|Android/i.test(navigator.userAgent)) {
     document.getElementById('phone-message').style.display = 'block';
-    document.getElementById('game-container').innerHTML = '';
+    document.getElementById('game-presentation').innerHTML = '';
 } else {
     document.getElementById('game-container').innerHTML = '<iframe src="https://squeishy.github.io/TankerzWebGame/"></iframe>';
 }
