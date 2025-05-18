bab287f3b082ca39a7957aa46265b01a9ea05ee5
Squeishy <gdlespainsaucho@gmail.com>
2025-05-19 00:01:00 +0200

    Crazy Cabbs

--- scripts/hide-iframe-for-phone-user.js
+++ scripts/hide-iframe-for-phone-user.js
@@ -2,10 +2,22 @@ if (/Mobi|Android/i.test(navigator.userAgent)) {
     document.getElementById('phone-message').style.display = 'block';
     document.getElementById('game-presentation').innerHTML = '';
 } else {
-    if (window.location.pathname.endsWith('tankerz-page.html')) {
-        document.getElementById('game-container').innerHTML = '<iframe src="https://squeishy.github.io/TankerzWebGame/"></iframe>';
+    const path = window.location.pathname;
+    let iframeSrc = '';
+
+    switch (true) {
+        case path.endsWith('tankerz-page.html'):
+            iframeSrc = 'https://squeishy.github.io/TankerzWebGame/';
+            break;
+        case path.endsWith('xarius-page.html'):
+            iframeSrc = 'https://squeishy.github.io/XariusWebGame/';
+            break;
+        case path.endsWith('crazy-cabs-page.html'):
+            iframeSrc = 'https://lusinecanard.fr/TESTCONTROLLER/CrazyCabs/index.html';
+            break;
     }
-    else if (window.location.pathname.endsWith('xarius-page.html')) {
-        document.getElementById('game-container').innerHTML = '<iframe src="https://squeishy.github.io/XariusWebGame/"></iframe>';
+
+    if (iframeSrc) {
+        document.getElementById('game-container').innerHTML = `<iframe src="${iframeSrc}"></iframe>`;
     }
 }
